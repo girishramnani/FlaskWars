6179ea97d50f8e44547f290f06813a44b6f17783
girishramnani <girishramnani95@gmail.com>
2015-05-23 21:41:35 +0650

    changed file app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask.ext.admin.base import Admin
 from flask.ext.admin.contrib.sqla.view import ModelView
 from flask import Flask
-from config import config
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+
+from config import config
+
 # from app.questions.model import Question,TestCase
 
 bootstrap = Bootstrap()
@@ -12,10 +14,11 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 from app.auth.model import User
-from app.questions.model import Question,TestCase
+from app.questions.model import Question, TestCase
+
 
 def create_app(config_name):
-    app=Flask(__name__)
+    app = Flask(__name__)
     bootstrap.init_app(app)
     db.init_app(app)
     admin = Admin(app)
@@ -24,9 +27,11 @@ def create_app(config_name):
     admin.add_view(ModelView(TestCase, db.session))
     app.config.from_object(config[config_name])
     from app.auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix='/auth')
+
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
     from app.questions import questions as questions_blueprint
-    app.register_blueprint(questions_blueprint,url_prefix="")
+
+    app.register_blueprint(questions_blueprint, url_prefix="")
     login_manager.init_app(app)
     print(app.url_map)
     return app
