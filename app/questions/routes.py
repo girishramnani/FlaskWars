afb206a4b9cb28e1063c98472ace25d098b27f60
girishramnani <girishramnani95@gmail.com>
2015-05-23 21:41:36 +0650

    changed file app/questions/routes.py

--- app/questions/routes.py
+++ app/questions/routes.py
@@ -1,25 +1,31 @@
+import os
+
 from flask.ext.login import current_user, login_required
+
 from flask.globals import request
 from flask.helpers import flash, url_for
 from flask import redirect
 from werkzeug.utils import secure_filename
+
 from app.questions.forms import SubmitForm
-import os
+
 __author__ = 'Girish'
 from flask import render_template
 from app.questions import questions
 from app.questions.model import Question
 
-ALLOWED_EXTENSIONS = set(['c', 'cpp', 'py', 'rb', 'java','txt'])
+ALLOWED_EXTENSIONS = set(['c', 'cpp', 'py', 'rb', 'java', 'txt'])
+
 
 @questions.route("/")
 def index():
     if current_user.is_authenticated():
         all_questions = Question.query.all()
         print(all_questions)
-        return render_template("index.html",all_quest=all_questions)
-    flash("you need to login to see the questions",category="warning")
+        return render_template("index.html", all_quest=all_questions)
+    flash("you need to login to see the questions", category="warning")
     return redirect(url_for("auth.login"))
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -28,9 +34,9 @@ def allowed_file(filename):
 @questions.route("/questions/<int:id>")
 @login_required
 def getquestion(id):
-    submitform =SubmitForm()
-    selected_question = Question.query.filter(Question.id ==id).first()
-    return render_template("question.html",question=selected_question,form = submitform)
+    submitform = SubmitForm()
+    selected_question = Question.query.filter(Question.id == id).first()
+    return render_template("question.html", question=selected_question, form=submitform)
 
 
 @questions.route('/questions/<int:id>/submit', methods=('GET', 'POST'))
@@ -45,9 +51,9 @@ def submit(id):
             filename = secure_filename(file.filename)
             file.save(os.path.join("app\\static\\upload", filename))
             flash("your code has been uploaded")
-            return redirect(url_for("questions.getquestion",id=id))
+            return redirect(url_for("questions.getquestion", id=id))
         else:
-            flash("that file format is not allowed",category="warning")
-            return redirect(url_for("questions.getquestion",id=id))
+            flash("that file format is not allowed", category="warning")
+            return redirect(url_for("questions.getquestion", id=id))
 
 
