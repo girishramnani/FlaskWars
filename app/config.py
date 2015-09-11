85c1756c8c56212acb05348cd366eb7a5e3e7096
girishramnani <girishramnani95@gmail.com>
2015-09-12 04:26:46 +0530

    added config

---
+++ app/config.py
@@ -0,0 +1,27 @@
+__author__ = 'Girish'
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SECRET_KEY = 'Top SECRET'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    RECAPTCHA_PARAMETERS = {'hl': 'zh', 'render': 'explicit'}
+    RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
+    #UPLOAD_FOLDER = os.path.join(basedir, 'app/static/upload')
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    'development': DevelopmentConfig
+}
